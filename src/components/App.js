f898950171b77215364a219ccd2d55adbb0b58bf
Ionut Colceriu <contact@ghinda.net>
2016-07-04 15:51:56 +0300

    Initial refactoring for the css styles. Remove semantic-ui so we have more control over the styles. Remove Museo, we'll use Source Sans Pro to be consistent with gorgias-docs, and have less overhead. Clean-up the navigation styles and fix the index links.

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,46 @@
 import React from 'react'
-import {Link} from 'react-router'
+import {Link, IndexLink} from 'react-router'
 import {fromJS} from 'immutable'
 import '../../static/css/main.less'
 import data from '../../data/openapi.json'
 
 const openapi = fromJS(data)
 
+import logo from '../../static/img/gorgias-logo-white-transparent.png'
+
 export const App = ({children}) => (
     <div>
         {/*   Navigation SideColumn   */}
         <div className="navigation">
-            <h1>Gorgias <span style={{ color: '#0d87dd'}}> API </span></h1>
-            <p>INTRODUCTION</p>
+            <IndexLink href="/" className="logo" to="/">
+              <img src={logo} alt="Gorgias"/>
+              <span>
+                API
+              </span>
+            </IndexLink>
+
+            <h2>INTRODUCTION</h2>
             <ul>
                 <li>
-                    <Link activeClassName="activeLink" to="/">Getting Started</Link>
+                    <IndexLink activeClassName="nav-item-active" to="/" className="nav-item">Getting Started</IndexLink>
                 </li>
             </ul>
 
-            <p>API</p>
+            <h2>API</h2>
             <ul>
                 {openapi.get('tags').map(tag => (
                     <li key={tag.get('name')}>
-                        <Link activeClassName="activeLink"
+                        <Link className="nav-item" activeClassName="nav-item-active"
                               to={`/api/${tag.get('name')}`}>{tag.get('name')}</Link>
                     </li>
                 )).toList().toJS()}
             </ul>
 
-            <p>DEFINITIONS</p>
+            <h2>DEFINITIONS</h2>
             <ul>
                 {openapi.get('definitions').map((def, name) => (
                     <li key={name}>
-                        <Link activeClassName="activeLink"
+                        <Link className="nav-item" activeClassName="nav-item-active"
                               to={`/definitions/${name}`}>{name}</Link>
                     </li>
                 )).toList().toJS()}
