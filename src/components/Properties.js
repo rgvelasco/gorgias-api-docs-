48cef8f5e2090008b58802a2ba35ef2a036d8dcd
Ionut Colceriu <contact@ghinda.net>
2016-07-05 16:05:16 +0300

    Redesign the Properties tables.

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -3,10 +3,24 @@ import React from 'react'
 const Property = ({prop, name, required}) => {
     return (
         <tr>
-            <td>{name}</td>
-            <td>{prop.get('type')}</td>
-            <td>{required && required.includes(name) ? 'required' : ''}</td>
-            <td>{prop.get('description')}</td>
+            <td>
+              <code className="properties-table-name">
+                {name}
+              </code>
+
+              {required && required.includes(name) ? (
+                <span className="properties-table-required">REQUIRED</span>
+              ) : null}
+            </td>
+            <td>
+              <span className="properties-table-type">
+                {prop.get('type')}
+              </span>
+
+              <p>
+                {prop.get('description')}
+              </p>
+            </td>
         </tr>
     )
 }
@@ -17,36 +31,19 @@ export const Properties = ({name, definition}) => {
     }
 
     return (
-        <div className="attributes">
-            <h2>{name} object properties:</h2>
-            <div className="tableCard">
-                {/*  Table for the Attributes of the object  */}
-                <table className="ui celled striped table" id="table">
-                    <thead>
-                    <tr>
-                        <th column="name">
-                            <strong>Name</strong>
-                        </th>
-                        <th column="type">
-                            <em>Type</em>
-                        </th>
-                        <th column="required">
-                            <em>Required</em>
-                        </th>
-                        <th column="description">
-                            <em>Description</em>
-                        </th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {
-                        definition.get('properties').map((prop, name) => (
-                            <Property key={name} prop={prop} name={name} required={definition.get('required')}/>
-                        )).toList()
-                    }
-                    </tbody>
-                </table>
-            </div>
+        <div className="properties">
+            <h2 className="properties-title">
+              {name} attributes
+            </h2>
+            <table className="properties-table">
+                <tbody>
+                {
+                    definition.get('properties').map((prop, name) => (
+                        <Property key={name} prop={prop} name={name} required={definition.get('required')}/>
+                    )).toList()
+                }
+                </tbody>
+            </table>
         </div>
     )
 }
