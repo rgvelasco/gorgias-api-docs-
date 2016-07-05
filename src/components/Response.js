55542c8cc5c65a498d5983f0002f81d60e0c52ab
Sebi Kovacs <sebi.kovacs@gmail.com>
2016-07-05 17:15:27 +0300

    Created new Response component

---
+++ src/components/Response.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import {fromJS} from 'immutable'
+import data from '../../data/openapi.json'
+const openapi = fromJS(data)
+
+export const Response = ({response}) => {
+    var props = null
+    var res
+
+    if (response.get('schema')) {
+        res = response.get('schema')
+    } else if (response.get('description')) {
+        res = response.get('description')
+    }
+
+    if (typeof res === 'object') {
+        if (res.get('$ref')) {
+            props = openapi.getIn(['definitions', res.get('$ref').replace('#/definitions/', ''), 'properties'])
+        }
+    }
+
+    return (
+        <div>
+            {props ? <ResponseTable properties={props} /> : <ResponseMessage message={res} />}
+        </div>
+    )
+}
+
+export const ResponseTable = ({properties}) => {
+    return (
+        <table>
+            <tbody>
+                {properties.map((a, b) => (
+                    <ResponseTableRow a={a} b={b} key={b}  />
+                )).toArray()}
+
+            </tbody>
+        </table>
+    )
+}
+
+export const ResponseTableRow = ({a,b}) => {
+    return (
+        <tr>
+            <td>{b}</td>
+            <td>{a.get('type')}</td>
+        </tr>
+    )
+}
+
+export const ResponseMessage = ({message}) => {
+    return (
+        <code className="code">{message}</code>
+    )
+}
