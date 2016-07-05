a38007f62a9f09aa55e57d56ee2d9bec4cc21773
Sebi Kovacs <sebi.kovacs@gmail.com>
2016-07-05 12:12:09 +0300

    Added styles for objects

--- src/components/Path.js
+++ src/components/Path.js
@@ -52,9 +52,31 @@ const Verb = ({verb, method, uri}) => {
     )
 }
 
+function syntaxHighlight(json) {
+    if (typeof json != 'string') {
+         json = JSON.stringify(json, undefined, 2);
+    }
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
+
 export const Response = ({response}) => {
     var props = null
-    let res
+    var res
 
     if (response.get('schema')) {
         res = response.get('schema')
@@ -73,12 +95,13 @@ export const Response = ({response}) => {
             props.map((desc, key) => {
                 obj[key] = desc.get('type')
             })
-            
+
+            obj = syntaxHighlight(obj)            
         }
     }
 
     return (
-        <div></div>
+        <pre dangerouslySetInnerHTML={{__html: obj}}></pre>
     )
 }
 
