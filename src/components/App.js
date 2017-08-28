9b982b259f6fdefe4d36d6c32f8c86dbc96a08a8
nedzen <nedzen.11@gmail.com>
2017-08-28 23:04:40 +0300

    removed unused css files and converted all stylesheets to sass

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 import '../static/css/main.css'
 import Navigation from './Navigation'
 
