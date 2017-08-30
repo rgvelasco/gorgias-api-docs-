e2b5a3c93f9c3134d8541576fcb0a113523ecb6b
nedzen <nedzen.11@gmail.com>
2017-08-30 12:25:05 +0300

    stash it

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 // we don't need bootstrap
 import '../static/css/main.css'
 import Navigation from './Navigation'
+// import '../static/js/jquery-3.2.1.slim.min.js'
+// import '../static/js/scrollspy.js'
 
 import {Intro} from './pages/Intro'
 import {Authentication} from './pages/Authentication'
@@ -18,7 +20,7 @@ export const App = () => {
 
             {/*   MAIN (everything except the Navigation SideColumn)   */}
             {/*  CONTENT (Tag or Definition)  */}
-            <div className="main">
+            <div data-spy="scroll" data-target="#navbar-example3" data-offset="0" className="main scrollspy-example-2">
                 <Intro/>
                 <Authentication />
                 <QueryingTheAPI />
